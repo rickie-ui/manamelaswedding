65361b5ec01e365262a3ac7736f516e50cfee4ae
Hlobisile Lukhele <n.thwala03@gmail.com>
2024-07-02 17:02:44 +0200

    created schedule page and added wedding venue information

---
+++ src/pages/Schedule.jsx
@@ -0,0 +1,26 @@
+
+
+const Schedule = () => {
+    return (
+      <div className="container md:mx-auto bg-gray ">
+        <h1 className="mt-4 mb-6 text-2x">Schedule</h1>
+        <p className="align-items">
+        Kindly arrive at 13:00 pm for your convenience, a chance to know fellow guests,
+        take pictures and to be seated as the wedding talk will start at 14:00 pm and the 
+        whole event will end at18:00 pm. Programme will be shared electronically before or on the day.
+        </p>
+        <p>There’s an open bar at the venue for your preferred alcoholic beverages.
+        It’s a no cash facility, only card swipes.
+        </p>
+        <p>The whole programme will be at Rosemary Hill, The Greenhouse Café. Click link to get directions.</p>
+        <button>Get Directions</button>
+        <p className="more-info">
+        For more information, kindly contact Wedding Coordinator 
+        </p>
+        <p className="contact-info">
+        Bell Mpebe:072 581 2840
+        </p>
+      </div>
+    )
+}
+export default Schedule;
