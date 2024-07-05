1a790df0167e615973816cd6572406cd0d567c65
Hlobisile Lukhele <n.thwala03@gmail.com>
2024-07-05 22:37:28 +0200

    added copyright information and styled it

--- src/shared/Footer.jsx
+++ src/shared/Footer.jsx
@@ -1,5 +1,17 @@
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="container Footer w-full h-auto p-6  font-bold text-lg">
+      <p className="text-black flex items-center">
+      <span className="copyright items-left justify-start">
+      Copyright © 2024  <span className="link"> <a href="https://themanswedding.com/">The MansWedding  </a></span> - All Rights Reserved.
+      </span>
+      <span className="developers items-right justify-end ml-40">
+        Developed by <a href="" className="text-white">Hlobisile Lukhele</a> & <a href="" className="text-white">Erick M</a>
+      </span>
+      </p>
+
+    </div>
+  ) 
 };
 
 export default Footer;
