afea335e736ba278cbdb32cbed6c04729aa7908c
Hlobisile Lukhele <n.thwala03@gmail.com>
2024-07-17 05:55:16 +0200

    added 3 nav links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,6 +8,9 @@ const Navbar = () => {
       <nav>
         <Link to={`/`}>Home</Link>
         <Link to={`story`}>Our Story</Link>
+        <Link to={`Schedule`}>Schedule</Link>
+        <Link to={`Accomodation`}>Accomodation</Link>
+        <Link to={`Registry`}>Registry</Link>
       </nav>
       <Outlet />
       <Footer />
