a15953b661276562b099c89f77837415206c7f3f
HLOBI <n.thwala03@gmail.com>
2024-07-19 06:32:46 -0700

    no updates

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,6 @@ import Story from "./Story.jsx"
 import Accommodation  from "./Accomodation.jsx"
 import Schedule from "./Schedule.jsx"
 
-
 const Home = () => {
   return <div className="Conatiner w-full h-uto ">
           <Story />
