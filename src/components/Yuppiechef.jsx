f40f39510c6de24397437d79f93914ec37f94d0c
HLOBI <n.thwala03@gmail.com>
2024-07-19 06:19:51 -0700

    created yuppiechef registery with links

---
+++ src/components/Yuppiechef.jsx
@@ -0,0 +1,16 @@
+import yuppiechef from "../Images/yuppiechef.png"
+
+
+const MrPriceRegistryLink = () => {
+  const registryUrl = "https://www.yuppiechef.com/registry.htm?action=view&orderid=7893952";
+
+  return (
+    <div className="registry-link flex justify-center">
+      <a href={registryUrl} target="_blank" rel="noopener noreferrer">
+        <img src={yuppiechef} alt="Yuppiechef Registry" className="w-full h-auto pt-8 pb-10 items-center" style={{ width: '800px', height: 'auto' }} />
+      </a>
+    </div>
+  );
+};
+
+export default MrPriceRegistryLink;
