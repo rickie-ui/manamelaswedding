a57294a0093f14560349f9c914b825a8e37cb3f4
HLOBI <n.thwala03@gmail.com>
2024-07-19 06:21:08 -0700

    imported the yuppiechef and mrprice registery and added some paragraph texts

--- src/pages/Registry.jsx
+++ src/pages/Registry.jsx
@@ -1,11 +1,26 @@
+import MrPriceRegistryLink from '../components/MrPriceRegistryLink'; 
+import Yuppiechef from '../components/Yuppiechef'; 
 
 
-const Registry = () => {
-    return (
-      <div>
-        Registry
+const App = () => {
+  return (
+    <>
+    <div className="container-fluid App bg-black text-white w-full h-full">
+      <h1 className='flex justify-center text-2xl font-sans font-medium pt-8 registry-heading'>Welcome to Our Registry</h1>
+      <p className="pt-4 text-xl flex justify-center font-medium m-10">
+      Your presence is a valued gift to us! However, if you wish to gift us, we have compiled a registry for your convenience and made alternative gifting arrangements as per below.
+      </p>
+      <p className='pt-4 text-xl flex justify-center font-medium m-11'>
+      We deeply appreciate these, and other thoughtful gifts of your choice.
+      Any voucher will be appreciated.
+      </p>
+      <div className='content m-4'>
+        <MrPriceRegistryLink />
+        <Yuppiechef />
       </div>
-    )
-  }
-  
-  export default Registry
+    </div>
+    </>
+  );
+};
+
+export default App;
