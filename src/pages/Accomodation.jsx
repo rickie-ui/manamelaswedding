7af15fd3f3f76b29730838ddf7d4584908e68b10
HLOBI <n.thwala03@gmail.com>
2024-07-19 06:32:02 -0700

    reduced font size

--- src/pages/Accomodation.jsx
+++ src/pages/Accomodation.jsx
@@ -6,7 +6,7 @@ const Accommodation = () => {
         <h1 className="flex justify-center pt-8 Heading text-4xl pb-6 text-white">Accommodation</h1>
         <div className="grid md:grid-cols-2 sm:grid-cols-1 gap-2 mb-8 items-center ">
         <div className="grid-item   justify-center pl-10"> 
-        <p className="text-2xl color-white pb-6 text-white md:p-4 sm:p-10">
+        <p className="text-xl color-white pb-6 text-white md:p-4 sm:p-10">
         The wedding venue has accommodation that's dependent on availability. Click link to have a view and book.
         </p>
         <button className="btn btn-primary btn-accom text-white rounded-md p-4 items-center md:ml-40  hover:bg-white hover:text-black">
@@ -16,7 +16,7 @@ const Accommodation = () => {
         </button>
         </div>
         <div className="grid-item 2nd-grid  md:pt-0 sm:pt-10 "> 
-        <p className="text-2xl color-white pb-6 text-white md:p-6 alt-para">
+        <p className="text-xl color-white pb-6 pt-4 text-white md:p-4 alt-para">
         Alternatively, you can book on Airbnb at any place near or surrounding the venue.
         </p>
         <button className="btn btn-primary bg-black btn-acc text-white rounded-md p-4 items-center text-bold md:ml-40  hover:bg-white hover:text-black hover:text-bold translate-x-2">
@@ -26,7 +26,7 @@ const Accommodation = () => {
       </button>
     </div>
   </div>
-        <p className="text-2xl flex items-center pl-8 md:pt-6 sm: text-white">Kindly arrange for personal transport as there's no public transport available to and from the venue.
+        <p className="text-xl flex items-center pl-8 md:pt-6 sm: text-white">Kindly arrange for personal transport as there's no public transport available to and from the venue.
            There's a tollagte at N4(Donkerhoek Boschkop),as you off-ramp into the dirt road leading to the farm.
            Kindly ensure you have cash or card  that swipes or taps, It's about R20.
         </p>
