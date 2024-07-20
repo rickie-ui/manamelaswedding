5bfe5bf730e1ff9204e291e2458b172cbdf5547e
Hlobisile Lukhele <n.thwala03@gmail.com>
2024-07-20 21:41:23 +0200

    created a gallery page with coming soon text

---
+++ src/pages/Gallery.jsx
@@ -0,0 +1,13 @@
+
+
+const Gallery = () => {
+  return (
+    <div className="relative w-full h-screen bg-cover bg-center" style={{ backgroundImage: `url('https://drive.google.com/file/d/1s_IGGOMkaxF-RzWtcmKaUSPLoqGLqwOo/view?usp=sharing')` }}>
+      <div className="absolute inset-0 bg-black bg-opacity-50 flex justify-center items-center">
+        <h1 className="text-white text-4xl md:text-6xl lg:text-8xl font-bold text-center">Gallery Coming Soon....</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Gallery;
