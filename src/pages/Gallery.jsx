39d3dcac08de3b9f58b7efabe794bf1258ebb13e
Hlobisile Lukhele <n.thwala03@gmail.com>
2024-07-20 21:42:22 +0200

    reduced the font size

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -4,7 +4,7 @@ const Gallery = () => {
   return (
     <div className="relative w-full h-screen bg-cover bg-center" style={{ backgroundImage: `url('https://drive.google.com/file/d/1s_IGGOMkaxF-RzWtcmKaUSPLoqGLqwOo/view?usp=sharing')` }}>
       <div className="absolute inset-0 bg-black bg-opacity-50 flex justify-center items-center">
-        <h1 className="text-white text-4xl md:text-6xl lg:text-8xl font-bold text-center">Gallery Coming Soon....</h1>
+        <h1 className="text-white text-2xl md:text-6xl lg:text-8xl font-bold text-center">Gallery Coming Soon....</h1>
       </div>
     </div>
   );
