fe6297e5955c8c529b2f6d8bed4ea130cf83bb35
HLOBI <n.thwala03@gmail.com>
2024-07-22 02:36:11 -0700

    centered inspiration button on mobile

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
       {/* hero section */}
       <div className="bg-[url('Images/hero.jpg')] bg-cover relative bg-no-repeat lg:h-[600px] h-[350px]">
         <div className='font-italiano text-4xl lg:text-5xl tracking-wider capitalize absolute lg:right-24 lg:top-32  top-52 right-14 z-10 opacity-80'>
-          We're getting <br /> married!!
+          We`&apos;`re getting <br /> married!!
         </div>
         <span className='absolute inset-0 bg-black/40'></span>
       </div>
